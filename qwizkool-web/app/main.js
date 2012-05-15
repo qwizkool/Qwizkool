887a333a72f7fa173b72d77e664733d61c430bd3
Vinod KG <kgvinod@gmail.com>
2012-05-15 00:58:51 -0700

    3 page nav with about_us

--- qwizkool-web/app/main.js
+++ qwizkool-web/app/main.js
@@ -15,15 +15,19 @@ require([
   "modules/showcase_topics",
   "modules/social_connection",
   "modules/registration",
-  "modules/footer"
+  "modules/footer",
+  "modules/about_us",  
+  "modules/view_utils"
+
 ],
 
-function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMain, ShowcaseTutorial, ShowcaseTopics, SocialConnection, Registration, Footer) {
+function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMain, ShowcaseTutorial, ShowcaseTopics, SocialConnection, Registration, Footer, AboutUs, ViewUtils) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
     routes: {
       "register": "register",      
+      "about_us": "about_us",      
       "": "index",
       ":hash": "index"
     },
@@ -38,7 +42,7 @@ function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMa
       // Attach the tutorial to the DOM
       front_page.render(function(el) {
         $("#main").html(el);
-
+/*
         // Fix for hashes in pushState and hash fragment
         if (hash && !route._alreadyTriggered) {
           // Reset to home, pushState support automatically converts hashes
@@ -50,7 +54,7 @@ function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMa
           // Set an internal flag to stop recursive looping
           route._alreadyTriggered = true;
         }
-        
+*/      
       });      
 	  
 	  
@@ -98,51 +102,20 @@ function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMa
     
     
     register: function(hash) {
-      
       //alert("register invoked");
-      
-      var route = this;
-      var front_page = new FrontPage.View();
+     
+      var user_register = new Registration.View();
+      ViewUtils.simple_view(user_register);
 
-      // Attach the tutorial to the DOM
-      front_page.render(function(el) {
-        $("#main").html(el);
-
-        // Fix for hashes in pushState and hash fragment
-        if (hash && !route._alreadyTriggered) {
-          // Reset to home, pushState support automatically converts hashes
-          Backbone.history.navigate("", false);
-
-          // Trigger the default browser behavior
-          location.hash = hash;
-
-          // Set an internal flag to stop recursive looping
-          route._alreadyTriggered = true;
-  
-        }
-
-
-      });      
-	  
-	  
-	  var header = new Header.View();
-      header.render(function(el) {
-        $("#header").html(el);
-      });
-
-      	  var user_register = new Registration.View();
-      user_register.render(function(el) {
-        $("#page_body").html(el);
-      });
-
-	  var footer = new Footer.View();
-      footer.render(function(el) {
-        $("#footer").html(el);
-      });
-
-
-    }  
+    },
     
+    
+     about_us: function(hash) {
+     //alert("register invoked");
+     
+      var about_us = new AboutUs.View();
+      ViewUtils.simple_view(about_us);
+    }
     
     
   });
@@ -160,6 +133,7 @@ function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMa
 
     // Trigger the initial route and enable HTML5 History API support
     Backbone.history.start({ pushState: false });
+  
   });
 
 
@@ -185,7 +159,6 @@ function(namespace, $, Tabs, Backbone, FrontPage, Header, SignInForm, QwizkoolMa
     }
   });
   
-  
-
-  
+    
+ 
 });
---
+++ qwizkool-web/app/modules/about_us.js
@@ -0,0 +1,44 @@
+define([
+  "namespace",
+
+  // Libs
+  "use!backbone"
+
+  // Modules
+
+  // Plugins
+],
+
+function(namespace, Backbone) {
+
+  // Create a new module
+  var AboutUs = namespace.module();
+
+  // AboutUs extendings
+  AboutUs.Model = Backbone.Model.extend({ /* ... */ });
+  AboutUs.Collection = Backbone.Collection.extend({ /* ... */ });
+  AboutUs.Router = Backbone.Router.extend({ /* ... */ });
+
+  // This will fetch the tutorial template and render it.
+  AboutUs.View = Backbone.View.extend({
+    template: "app/templates/about_us.html",
+
+    render: function(done) {
+      var view = this;
+
+      // Fetch the template, render it to the View element and call done.
+      namespace.fetchTemplate(this.template, function(tmpl) {
+        view.el.innerHTML = tmpl();
+
+        // If a done function is passed, call it with the element
+        if (_.isFunction(done)) {
+          done(view.el);
+        }
+      });
+    }
+  });
+
+  // Required, return the module for AMD compliance
+  return AboutUs;
+
+});
